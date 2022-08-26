e16aabfbea2d517b761ea3de7c7c00f91c99e2a3
Lewis, Mekhi D <lewismed@iu.edu>
2022-08-26 17:58:31 -0400

    js update

--- scriptss.js
+++ scriptss.js
@@ -1,21 +1,61 @@
-console.log("hr")
-
-
 const api=(()=>{
 
     baseUrl='http://localhost:4232/posts/1'
     todospath='todos ';
 
 getTodos=()=>{
-fetch([this.baseUrl,this.todospath].join('/'))
-   .then(response => response.json())
-   .then(json => console.log(json))
-};
+fetch([this.baseUrl,this.todospath].join('/')).then(response => response.json())
+  //.then(json => console.log(json))
+
 deleteTodo=(id)=>{};
-return {getTodos};
+return {getTodos,
+deleteTodo};
 })()
 
-api.getTodos()
+api().getTodos( )
+
+
+
+/* ~~~~~~~~~~~~~~~~ View ~~~~~~~~~~~~~~~~ */
+const view=(()=>{
+
+   return{}
+  
+   })
+  
+  
+
+
+/* ~~~~~~~~~~~~~~~~ Model ~~~~~~~~~~~~~~~~ */
+
+
+const model=((api)=>{
+   const {getTodos,deleteTodo=api;
+   //const deleteTodo=api.deleteTodo;
+
+   return{
+
+getTodos,
+deleteTodo
+
+   }
+  
+   })(api)
+  
+  
+
+
+/* ~~~~~~~~~~~~~~~~ Controller ~~~~~~~~~~~~~~~~ */
+
+const controller=(()=>{
+const init=()={
+   model.getTodos
+}
+
+
+   return{}
+  
+   })()
 /*
 class Api{
 
