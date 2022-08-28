e490a794821648919cdfe50ef5821f4e905c0aa2
Lewis, Mekhi D <lewismed@iu.edu>
2022-08-28 12:47:53 -0400

    index4.js updae

--- script2.js
+++ script2.js
@@ -24,14 +24,14 @@ deleteTodo,};
 
 /* ~~~~~~~~~~~~~~~~ View ~~~~~~~~~~~~~~~~ */
 const View=(()=>{
-    const domstr={classlist:"#nonselected_containter",deletebtn:'.deletebtn'}
+    const domstr={classlist:"#nonselected_containter",
+    courseId:'.course_id'}
+
+
 const render=(ele,tmp)=>{
     ele.innerHTML=tmp
+    console.log(ele)
 }
-
-
-
-
 
 
 
@@ -44,24 +44,27 @@ const createTmp=arr=>{
             }
         
         
-        tmp+=`              <li>
+        tmp+=`              <li class ='course_id' id="${todo.courseId}>
        <span>${todo.courseName}</span><br>
         <span>${todo.credit}</span><br>
        <span>${todo.required}</span><br>
        <span>${todo.courseId}
        
        
-       </span><br>
-       <button class ='deletebtn' id="${todo.courseId}>X</button>
+       </span>
+    
       
       </li>
 
             
         `
+     
     })
+    
     return tmp;
 }
 
+console.log(createTmp.todo)
    return{render,
     domstr,
     createTmp
@@ -104,34 +107,95 @@ deleteTodo
 /* ~~~~~~~~~~~~~~~~ Controller ~~~~~~~~~~~~~~~~ */
 
 const controller=((model,view)=>{
+
+
+
+
+
+
+
+
+
+    
 const init=()=>{
     const todolist=document.querySelector(view.domstr.classlist)
-console.log(todolist)
+    //console.log(view.createTmp.tmp)
+//const check=document.querySelector(view.)
     //console.log(todolist)
+    //console.log(model.getTodos())
    model.getTodos().then(todos=>{
     const tmp=view.createTmp(todos);
    view.render(todolist,tmp)
+//console.log(view.createTmp(todo))
 
-
-
+   
    //const deleteTodo = () => {
-    const deletebtns = document.querySelectorAll(view.domstr.deletebtn);
-deletebtns.forEach(btn=>{btn.addEventListener('click', event => {
-      const nwetodos=todos.filter(ele=> +ele.courseId!==+event.target.courseId);
-      const tmp=view.createTmp(nwetodos);
-      view.render(todolist,tmp)
+       const selectBtn=()=>{
+    const courseids = document.querySelector(view.domstr.courseId);
+    courseids.addEventListner("click",(e)=>{
+        if (totalnum<18){
+            let lis=document.getElementsByClassName('name')[0].innerHTML
+            let t=lis[i]=inner.html;
+            selectedCourses.forEach((selCourse)=>{
+                if (selCourse.courseName===name)
+                lis[i].parentNode.removeChild(lis[i])})
+
+
+            }
+        }
+    }
+
+
+
+    const courseids = document.querySelector(view.domstr.courseId);
+    courseids.addEventListner("click",(e)=>{
+    if (e.target.classList.contains("course_id") {
+        let classList = e.target.classList;
+        let course = state.courseList.find(
+          (elem) => elem.courseId == e.target.id
+        );
+
+        if (classList.contains("selected")) {
+          selectedCourses = selectedCourses.filter((elem) => {
+            return elem.courseId != course.courseId;
+          });
+          totalcredit -= course.credit;
+          e.target.classList.remove("selected");
+        } else {
+
+
+
+
+
+
+
+    console.log(courseids)
+    //let btn=getElementByTagName
+   // ("button")
+    courseids.forEach((btn=>{
+        btn.addEventListner('click',e=>{
+           console.log(e.target.courseId)
+        }// target is where the event is triggered aka where we click
+    )
+
+   }))})}
+//deletebtns.forEach(btn=>{btn.addEventListener('click', event => {
+    //  console.log(event.target.courseId)
+    
+   // const nwetodos=todos.filter(ele=> +ele.courseId!==+event.target.courseId);
+      //console.log(nwetodos)
+      //const tmp=view.createTmp(nwetodos);
+      //view.render(todolist,tmp)
 
 
       //if (event.target.className === 'deletebtn') {
        // state.todolist = state.todolist.filter(
           //(todo) => +todo.id !== +event.target.id
        // );
-      })
+     // })
      // model.deleteTodo(+event.target.id);
-    })//);
-   })};
-
-
+    //})//);
+   //})});
 
 
 
@@ -158,6 +222,9 @@ deletebtns.forEach(btn=>{btn.addEventListener('click', event => {
    })(Model,View)
  
    controller.init();
+
+   let idcheck=document.querySelectorAll(".deletebtn").innerText
+console.log(idcheck)
 /*
 class Api{
     baseUrl='http://localhost:4232/posts/1'
