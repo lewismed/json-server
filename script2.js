c47a374c9ca509934639f489eec7c1998582143c
Lewis, Mekhi D <lewismed@iu.edu>
2022-08-28 08:29:27 -0400

    compolusry vs elective

--- script2.js
+++ script2.js
@@ -38,13 +38,20 @@ const render=(ele,tmp)=>{
 const createTmp=arr=>{
     let tmp=''
     arr.forEach(todo=>{
+        if (todo.required==true){
+            todo.required="compulsory"}else{
+                todo.required="Elective"
+            }
+        
+        
         tmp+=`              <li>
        <span>${todo.courseName}</span><br>
         <span>${todo.credit}</span><br>
        <span>${todo.required}</span><br>
-       <span>${todo.courseId}</span><br>
+       <span>${todo.courseId}
        
-      
+       
+       </span><br>
        <button class ='deletebtn' id="${todo.courseId}>X</button>
       
       </li>
@@ -99,22 +106,48 @@ deleteTodo
 const controller=((model,view)=>{
 const init=()=>{
     const todolist=document.querySelector(view.domstr.classlist)
-
+console.log(todolist)
     //console.log(todolist)
    model.getTodos().then(todos=>{
     const tmp=view.createTmp(todos);
    view.render(todolist,tmp)
 
 
-   const deletebtns=document.querySelectorAll(view.domstr.deletebtn);
+
+   //const deleteTodo = () => {
+    const deletebtns = document.querySelectorAll(view.domstr.deletebtn);
+deletebtns.forEach(btn=>{btn.addEventListener('click', event => {
+      const nwetodos=todos.filter(ele=> +ele.courseId!==+event.target.courseId);
+      const tmp=view.createTmp(nwetodos);
+      view.render(todolist,tmp)
+
+
+      //if (event.target.className === 'deletebtn') {
+       // state.todolist = state.todolist.filter(
+          //(todo) => +todo.id !== +event.target.id
+       // );
+      })
+     // model.deleteTodo(+event.target.id);
+    })//);
+   })};
+
+
+
+
+
+   /*const deletebtns=document.querySelectorAll(view.domstr.deletebtn);
    console.log(deletebtns)
    deletebtns.forEach(btn =>{
-       btn.addEventListner()
+       btn
+       
+       .addEventListner('click',e=>{
+           console.log(e.target.courseId)
+       })
     })
 
    }
    )
-}
+}*/
 
 
 
